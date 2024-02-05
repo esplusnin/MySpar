a27c2613cac78c893995f5219a5751206ebbc093
esplusnin <esplusnin94@yandex.ru>
2024-02-05 10:40:51 +0400

    Initial Commit

---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Евгений on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
