b331a1a609bb81e964201f99cf4d51738b80bb23
esplusnin <esplusnin94@yandex.ru>
2024-02-05 16:11:35 +0400

    №4 Сверстать презентационное вью с картинкой о товаре

--- MySpar/Scenes/ContentView.swift
+++ MySpar/Scenes/ContentView.swift
@@ -3,12 +3,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-            Text("Hello, world!")            
+            GoodDetailNavigationView()
+            
+            GoodPresentationView()
+            
+            Spacer()
         }
-        .padding()
-        
     }
 }
 
