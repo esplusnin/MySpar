7a976d28d074d282bfb6de573b59fcd129efb5a3
esplusnin <esplusnin94@yandex.ru>
2024-02-05 11:22:31 +0400

    №4 Добавить шрифты

---
+++ MySpar/Extensions/UIFont + Extension.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+extension Font {
+    static let largeTitle = Font.system(size: 24, weight: .bold, design: .rounded)
+    static let mediumTitle = Font.system(size: 20, weight: .bold, design: .rounded)
+    static let smallTitle = Font.system(size: 16, weight: .bold, design: .rounded)
+    
+    static let bodyMedium = Font.system(size: 18, weight: .medium)
+    static let bodyRegular = Font.system(size: 16, weight: .regular)
+    static let bodySmall = Font.system(size: 14, weight: .regular)
+}
